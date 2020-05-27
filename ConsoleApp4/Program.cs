58c6168d09a053c743090ef18bf08de3fa1e53d9
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-05-27 22:56:55 +0300

    dasda

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartOyunuödevdeneme2
+{
+    class Program
+    {
+        public static Boolean İsCardOkey(string input)
+        {
+            if (input.Length != 2)
+            {
+                Console.WriteLine("Wrong İnput pick your card from cardlist!! ");
+                return false;
+
+            }
+            else if (!(input.StartsWith("K")) && !(input.StartsWith("M")) && !(input.StartsWith("S")) && !(input.StartsWith("R")))
+            {
+                Console.WriteLine("Wrong İnput pick your card from cardlist!! ");
+                return false;
+            }
+            else if (!(input.EndsWith("1")) && !(input.EndsWith("2")) && !(input.EndsWith("3")) && !(input.EndsWith("4")) && !(input.EndsWith("5")) && !(input.EndsWith("D")))
+            {
+                Console.WriteLine("Wrong İnput pick your card from cardlist!! ");
+                return false;
+            }
+            else if (opencard != "" && input != "RD" && input.Substring(0, 1) != opencard.Substring(0, 1) && input.Substring(1, 1) != opencard.Substring(1, 1))
+            {
+                Console.WriteLine("Wrong İnput pick your card from cardlist!! ");
+                return false;
+            }
+            return true;
+        }
+        public static string opencard = "";  //YERDEKİ KARTI GÖSTEREBİLMEMİZ İÇİN YAZILMIŞ STATİK FONKSİYON YERDEKİ KARTI VERİR
+        static void Main(string[] args)
+        {
+
+            string[] card =
+            {"K1","K2", "K3", "K4", "K5", "M1", "M2", "M3", "M4", "M5", "S1", "S2", "S3", "S4", "S5", "RD", "RD", "RD"}; //KART ARRAYİ
+            List<string> card2 = new List<string>();
+            for (int i = 0; i < 18; i++)
+            {
+                card2.Add(card[i]);    //ARRAYDAKİ KARTLARI LİSTE ATAN DÖNGÜ
+            }
+            Random random = new Random();
+            gamer Gamer1 = new gamer("gamer1");      // OYUNCU LİSTELERİ İÇİN PARAMETRELER
+            gamer Gamer2 = new gamer("gamer2");
+            gamer Gamer3 = new gamer("gamer3");
+            List<gamer> GamerList = new List<gamer>();
+            GamerList.Add(Gamer1);       //OYUNCU LİSTELERİNE EKLENEN OYUNCULAR
+            GamerList.Add(Gamer2);
+            GamerList.Add(Gamer3);
+
+            foreach (gamer Gamer in GamerList)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    int r = random.Next(0, card2.Count);    //OYUNCULARA RASTGELE 6 KART ATAN DÖNGÜ ÜST FOREACH DÖNGÜSÜ GAMERLERİ SIRALIYOR İÇ FOR DÖNGÜSÜ KARTLARI DAĞITIYOR
+                    Gamer.cards.Add(card2[r]);
+                    card2.Remove(card2[r]);
+                }
+
+            }
+
+            Gamer3.show();
+            Console.WriteLine();     //BİZDEKİ KARTLARI GÖSTEREN KODLAR
+            int a = random.Next(0, 3);
+
+            if (a == 0) //
+            {
+                Console.WriteLine("open lap/ turn gamer1:");  //AÇILIŞ TURUNDA KİMİN BAŞŞLAYACAĞINA KARAR VEREN RANDOM KOŞULLARI
+                opencard = Gamer1.givecard();
+            }
+            else if (a == 1)
+            {
+                Console.WriteLine("open lap/ turn gamer2:");
+                opencard = Gamer2.givecard();
+            }
+            else
+            {
+                Console.WriteLine("open lap/ turn gamer3:");
+                string input = "-1";
+                while (opencard != input)
+                {
+                    Console.WriteLine("Please select your cards:(First card cant be 'RD' ) (Forexample: M5 /S4 /K3) ");
+                    input = Console.ReadLine();
+                    if (input != "RD" && Program.İsCardOkey(input))
+                    {
+                        for (int i = 0; i < Gamer3.cards.Count; i++)
+                        {
+
+                            if (Gamer3.cards[i] == input) // GAMER 3 BİZİZ BİZ GİRİŞ YAPARAK OYNUYORUZ ,YERE ATTIĞIMIZ KARTI DA LİSTEDEN SİLEN KOŞUL
+                            {
+                                opencard = input;
+                                Gamer3.cards.Remove(input);
+                                break;
+
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+            Console.WriteLine("open card: {0}", opencard);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("---------------------------------------------------------");
+            int draw = 0;
+            string oldopencard = opencard;
+            while (true)//system exit kodu burda
+            {
+                if (draw >= 3)
+                {
+
+                    Console.WriteLine("Result :Draw");
+                    break;
+
+                }
+                Gamer3.show();
+                Console.WriteLine("Old open card is: " + opencard); //BİZİM DİĞER TURLARI OYNAMAMIZ İÇİN GİRİŞ ALAN WHİLE DÖNGÜSÜ
+                a++;
+                if (a > 2) //OYUNCULARIN SIRAYLA OYNAMASINI SAĞLAYAN KOŞUL
+                {
+                    a = 0;
+                }
+                if (Gamer1.isEmpty() || Gamer2.isEmpty() || Gamer3.isEmpty()) //EĞER BİRİNDE KART BİTERSE KAZANIR  BU KOŞUL SAĞLANIRSA OYUN BİTER
+                {
+                    break;
+                }
+
+                if (a == 2) //SIRANIN BİZE GELDİĞİNİ BİLDİREN KOŞUL
+                {
+                    Console.WriteLine("Your turn gamer3:");
+
+                    string input = "-1";
+                    while (opencard != input)
+                    {
+                        Console.WriteLine("Please  select your cards: (Forexample: K3 /M5 /S2) ");
+                        input = Console.ReadLine();
+                        if (input == "PAS") // EĞER ELİMİZDE YERE ATACAK DOĞRU KART YOKSA PPAS GEÇMEE KOŞULUMUZ
+                        {
+                            break;
+                        }
+
+                        if (input == opencard && GamerList[a].İsCardExist(input) == false)
+                        {
+                            input = "-1";
+                            continue;
+                        }
+
+
+                        if (Program.İsCardOkey(input))
+                        {
+                            for (int i = 0; i < Gamer3.cards.Count; i++)
+                            {
+
+                                if (Gamer3.cards[i] == input) //GİRİŞ YAPTIĞIMIZ KART KOŞULU
+                                {
+                                    if (input == "RD") // GİRDİĞİMİZ KART RD KARTIYSA YENİ RENK SEÇMEMİZE YARAYAAN KOŞUL
+                                    {
+                                        Console.WriteLine("Please choose new colour: (Forexample: K / S / M)  ");
+                                        string input2 = Console.ReadLine();
+                                        while (true)
+                                        {
+                                            if (input2 == "K" || input2 == "S" || input2 == "M")
+                                            {
+                                                break;
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("chosen colour did not found!!");
+                                                Console.WriteLine("Please choose new colour: (Forexample: K / S / M)  ");
+
+                                                input2 = Console.ReadLine();
+                                            }
+                                        }
+                                        opencard = input2 + opencard.Substring(1, 1);
+                                        input = opencard;
+                                        Console.WriteLine("RD card is used now:");
+                                        Gamer3.cards.Remove(Gamer3.cards[i]);
+                                        break;
+                                    }
+                                    opencard = input; //EĞER GİRİLEN KART RD DEĞİLSE  SEÇTİĞİMİZ KARTI YENİ YERDEKİ KART YAPAR YERE ATTIĞIMIZ KARTI LİSTEMİZDDEN SİLER
+                                    Gamer3.cards.Remove(input);
+                                    Console.WriteLine("open card: {0}", opencard);
+                                    break;  // EĞER GİRİŞ YAPTIYSAK  VE GİRİŞ KABUL EDİLMİŞSE TEKRAR TEKRAR GİRİŞ İSTEMEMESİ İÇİN BİZİ DÖNGÜDEN ÇIKARIR
+
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Your turn: " + GamerList[a].name); //SIRA BİZDE OLMAYINCA SIRANIN KİMDE OLDUĞUNU GÖSTEREN VE O AN OYUNU OYNAYAN KULLANICININ KARTLARINI GÖSTEREN KODLAR
+                                                                          // GamerList[a].show();
+
+                    opencard = GamerList[a].givecard();
+                    Console.WriteLine("open card: {0}", opencard);
+                }
+                Console.WriteLine("---------------------------------------------------------");
+                Console.WriteLine("---------------------------------------------------------");
+                Console.WriteLine("---------------------------------------------------------");
+
+                if (oldopencard == opencard)
+                {
+                    draw++;
+                }
+            }
+
+
+            Console.WriteLine("GAME FİNİSHED");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp4/gamer.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace KartOyunuödevdeneme2
+{
+    class gamer
+    {
+        public string name; //İSİM VE KARTLARI TUTACAĞIMIZ LİSTE
+        public List<string> cards = new List<string>();
+
+
+        public gamer() //OYUNCU ADLARINI TUTAN FONKSİYON
+        {
+            this.name = null;
+        }
+
+
+
+        public gamer(string name)
+        {
+            this.name = name;
+
+        }
+
+        public Boolean İsCardExist(string input)
+        {
+            for (int i = 0; i < this.cards.Count; i++)
+            {
+                if (input == this.cards[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Boolean isFull()   // OYUNCULARA DAĞITILAN 6 KARTIN TAMAMININ DAĞITILIP DAĞITIMADIĞINI KONTROL EDİYOR
+        {
+            if (this.cards.Count == 6)
+            {
+                return true;
+            }
+            return false;
+        }
+        public Boolean isEmpty() // KAZANANI BELİRTMEK İÇİN OYUNCULARIN KART LİSTELERİNİN BOŞ OLUP OLMADIĞINI KONTROL EDEN FONKSİYON
+        {
+            if (this.cards.Count == 0)
+            {
+                Console.WriteLine("WİNNER  İS:{0}", this.name);
+                return true;
+            }
+            return false;
+        }
+
+
+
+
+        public void show()   //KARTLARI GÖSTEREN FONKSİYON
+        {
+            Console.WriteLine("{0} CARDS: ", this.name);
+            foreach (string item in cards)
+            {
+
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public string LotColour()   // ELİMİZDE EN ÇOK HANGİ RENKTEN KART OLDUĞUNU BULMAYA YARAYAN FONKSİYON
+        {
+
+
+            int m = 0, k = 0, s = 0;
+            for (int i = 0; i < cards.Count; i++)
+            {
+                if (cards[i].StartsWith("M"))    // HANGİSİNİN ÇOK OLDUĞUNU BULMAK İÇİN YUKARDA TANIMLANMIŞ SAYILARI ARTTIRIYOR
+                {
+                    m++;
+                }
+                else if (cards[i].StartsWith("K"))
+                {
+                    k++;
+                }
+                else if (cards[i].StartsWith("S"))
+                {
+                    s++;
+
+                }
+
+
+            }
+            if (k >= m && k >= s)   //ÇOK OLAN KARTI DÖNDÜREN KOŞULLAR
+            {
+                return "K";
+            }
+            else if (m >= k && m >= s)
+            {
+                return "M";
+            }
+            else if (s >= m && s >= k)
+            {
+                return "S";
+
+            }
+
+            return "K";
+        }
+
+
+        public string givecard()  // KART ATMA FONKSİYONUMUZ
+        {
+            if (Program.opencard == "") //en çok bulunan kartı atar ortada kart yokkende elde bulunan en fazla aynı renkte kartı döndürür
+            {
+                for (int i = 0; i < cards.Count; i++)
+                {
+                    if (cards[i].StartsWith(LotColour()))  //MAANTIKLI OLMASI İÇİN EN BİLGİSAYARIN KONTROL ETTİĞİ LİSTELERDEN EN ÇÇOK OLAN KARRTI  İLK ATMASI İÇİN KOŞUL 
+                    {
+                        string cd = cards[i];
+                        cards.Remove(cards[i]);
+                        return cd;
+                    }
+                }
+            }
+            else // yerdeki karta göre kart atma
+            {
+                for (int i = 0; i < cards.Count; i++)
+                {
+                    if (Program.opencard.Substring(1, 1) == cards[i].Substring(1, 1))  //OPEN CARD İLK DEFA GÖRÜNTÜLENMİYORSA ÖNCELİKLE SONDAKİ SAYILARA GÖRE KART ATTIRAN KOŞUL
+                    {
+                        string cd = cards[i];
+                        cards.Remove(cards[i]);
+                        return cd;
+                    }
+
+
+                }
+                for (int i = 0; i < cards.Count; i++)
+                {
+
+                    if (Program.opencard.Substring(0, 1) == cards[i].Substring(0, 1))
+                    {
+                        string cd = cards[i];
+                        cards.Remove(cards[i]);
+                        return cd;
+                    }
+
+
+                }
+                for (int i = 0; i < cards.Count; i++)
+                {
+
+
+                    if (cards[i] == "RD")   //RENK DEĞİŞTİR KARTINI ÖNCEKİ KARTI VE YENİ SEÇİLEN RENKTEKİ KARTLARI GÖSTEREN KOŞULLAR
+                    {
+                        Console.WriteLine(this.name + "  Changed opencard " + Program.opencard + "  As " + LotColour() + Program.opencard.Substring(1, 1));
+                        Program.opencard = LotColour() + Program.opencard.Substring(1, 1);
+                        cards.Remove(cards[i]);
+                        return Program.opencard;
+
+                    }
+
+                }
+
+                Console.WriteLine("PAS GEÇİLDİ");
+            }
+            return Program.opencard;
+        }
+
+
+    }
+}
